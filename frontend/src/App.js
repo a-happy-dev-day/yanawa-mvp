f2530d4fd2cc32e8d242fe3c758885ee32ba4990
stoneboat <tood2111@gmail.com>
2022-04-23 15:24:32 +0900

    reset css & font 설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import { Reset } from "styled-reset";
+
 import HobbyChoice from "./pages/HobbyChoice";
 import LevelCheck from "./pages/LevelCheck";
 import LevelYear from "./pages/LevelYear.js";
@@ -7,6 +9,7 @@ import LevelYear from "./pages/LevelYear.js";
 function App() {
   return (
     <div>
+      <Reset />
       <Routes>
         <Route path='/hobbychoice' element={<HobbyChoice />} />
         <Route path='/levelyear' element={<LevelYear />} />
